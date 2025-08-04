afe837bf4703b96419692e8e4932f660560ba5a0
Siam Haque <siamhoq@gmail.com>
2025-08-04 09:08:44 +0600

    updated

--- problem03.js
+++ problem03.js
@@ -2,20 +2,6 @@
 
 var lastDay = 30 ;
 
-// var day = 1;
-// while(day<=lastDay){
-   
-//     if(day%3===0){
-//         console.log(day+" - medicine")
-//     }
-
-//     if(day%3!=0){
-//             console.log(day+" - rest");
-//     }
-    
-//     day++;
-// }
-
 for (let day =1 ; day<=lastDay ; day++){
     if(day%3===0){
         console.log(day+" - medicine")
